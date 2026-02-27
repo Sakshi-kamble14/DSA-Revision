4cad72dd0d92232517dbcb27cf34e80012ead34d
Sakshi Kamble <sakshikamble300@gmail.com>
2026-02-27 17:00:12 +0000

    Q5

---
+++ Searching/src/com/sunbeam/Assignment01_Q5.java
@@ -0,0 +1,15 @@
+package com.sunbeam;
+
+import java.util.Scanner;
+
+public class Assignment01_Q5 {
+    public static void main(String[] args) {
+        System.out.println("Enter the array size-");
+        Scanner sc=new Scanner(System.in);
+        int size;
+        size=sc.nextInt();
+        int []arr=new int[size];
+        System.out.println("Enter the array elements");
+    }
+    
+}
