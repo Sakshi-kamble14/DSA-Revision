88bddc4c2a7271ec1fece80da38367880f7807ab
Sakshi Kamble <sakshikamble300@gmail.com>
2026-03-11 07:33:02 +0530

    Remove the unnecessary comparison

--- Sorting/src/com/sunbeam/BubbleSort.java
+++ Sorting/src/com/sunbeam/BubbleSort.java
@@ -9,13 +9,34 @@ public class BubbleSort {
 		for(int i=1;i<arr.length;i++) {
 			pass++;
 			for(int j=0;j<arr.length-1;j++) {
+				comps++;
 				if(arr[j]>arr[j+1]) {
 					int temp=arr[j];
 					arr[j]=arr[j+1];
 					arr[j+1]=temp;
+					
 				}
 			}
 		}
+		System.out.println("Passes-"+pass);
+		System.out.println("Comparison-"+comps);
+	}
+	public static void improvedbubblesort(int []arr,int left,int right) {
+		int pass=0,comps=0;
+		for(int i=1;i<arr.length;i++) {
+			pass++;
+			for(int j=0;j<arr.length-i;j++) {
+				comps++;
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+					
+				}
+			}
+		}
+		System.out.println("Passes-"+pass);
+		System.out.println("Comparison-"+comps);
 	}
 	
 	public static void display(int []arr) {
@@ -34,15 +55,27 @@ public class BubbleSort {
 		for(int i=0;i<size;i++) {
 			arr[i]=sc.nextInt();
 		}
+		
 		System.out.print("Before sorting: ");
 		display(arr); 
 		System.out.println(); 
-
+		
 		bubblesort(arr, 0, size - 1);
+		
+		System.out.print("After sorting: ");
+		display(arr);
+		
+		System.out.println();
+		
+		System.out.print("Before sorting: ");
+		display(arr); 
+		System.out.println(); 
+		
+		improvedbubblesort(arr, 0, size-1);
 
 		System.out.print("After sorting: ");
 		display(arr);
-
+		
 	}
 
 }
