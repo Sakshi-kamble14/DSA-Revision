fd71255a168574fabd3e29a0638e1f089fe37919
Sakshi Kamble <sakshikamble300@gmail.com>
2026-03-11 07:18:09 +0530

    Sort array using Bubble Sort

---
+++ Sorting/src/com/sunbeam/BubbleSort.java
@@ -0,0 +1,48 @@
+package com.sunbeam;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+	public static void bubblesort(int []arr,int left,int right) {
+		int pass=0,comps=0;
+		for(int i=1;i<arr.length;i++) {
+			pass++;
+			for(int j=0;j<arr.length-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+	}
+	
+	public static void display(int []arr) {
+		for(int i=0;i<arr.length;i++) 
+			System.out.print(arr[i]+" ");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int size;
+		System.out.println("Enter size of array-");
+		size=sc.nextInt();
+		int[] arr=new int[size];
+		System.out.print("Enter array-");
+		for(int i=0;i<size;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.print("Before sorting: ");
+		display(arr); 
+		System.out.println(); 
+
+		bubblesort(arr, 0, size - 1);
+
+		System.out.print("After sorting: ");
+		display(arr);
+
+	}
+
+}
